f2b3c8b6f2d84188584053ec6666af7c4389476b
Pedro Ramos <pedrotramos97@gmail.com>
2021-04-30 15:08:45 -0300

    Aula 14 feita

--- material/aulas/13-paralelismo-tarefas-dados/pi-numeric-integration.cpp
+++ material/aulas/13-paralelismo-tarefas-dados/pi-numeric-integration.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include <chrono>
+#include <omp.h>
 
 static long num_steps = 1000000000;
 double step;
 
-int main() {
-    int i;
-    double x, pi, sum = 0.0;
+int main()
+{
+    int i, j;
+    double x1, x2, pi, sum1, sum2 = 0.0;
     step = 1.0 / (double)num_steps;
 
     auto start_time = std::chrono::high_resolution_clock::now();
-    for (i = 0; i < num_steps; i++) {
-        x = (i + 0.5) * step;
-        sum = sum + 4.0 / (1.0 + x * x);
+#pragma omp parallel
+    {
+#pragma omp master
+        {
+#pragma omp task
+            {
+                for (i = 0; i < num_steps / 2; i++)
+                {
+                    x1 = (i + 0.5) * step;
+                    sum1 = sum1 + 4.0 / (1.0 + x1 * x1);
+                }
+            }
+#pragma omp task
+            {
+                for (j = num_steps / 2; j < num_steps; j++)
+                {
+                    x2 = (j + 0.5) * step;
+                    sum2 = sum2 + 4.0 / (1.0 + x2 * x2);
+                }
+            }
+        }
     }
 
-    pi = step * sum;
+    // for (i = 0; i < num_steps; i++)
+    // {
+    //     x1 = (i + 0.5) * step;
+    //     sum1 = sum1 + 4.0 / (1.0 + x1 * x1);
+    // }
+
+    pi = step * (sum1 + sum2);
     auto end_time = std::chrono::high_resolution_clock::now();
-    auto runtime = std::chrono::duration_cast<std::chrono::seconds> (end_time - start_time);
-    
+    auto runtime = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+
     std::cout << "O valor de pi calculado com " << num_steps << " passos levou ";
     std::cout << runtime.count() << " segundo(s) e chegou no valor : ";
     std::cout.precision(17);
--- material/aulas/14-exercicios/pi_recursivo2.cpp
+++ material/aulas/14-exercicios/pi_recursivo2.cpp
@@ -5,15 +5,12 @@ static long num_steps = 1024l * 1024 * 1024 * 2;
 
 #define MIN_BLK 1024 * 1024 * 256
 
-double sum = 0;
-
-void pi_r(long Nstart, long Nfinish, double step)
+double pi_r(long Nstart, long Nfinish, double step, double sum)
 {
     long i, iblk;
+    double sum1, sum2;
     if (Nfinish - Nstart < MIN_BLK)
     {
-#pragma omp parallel for reduction(+ \
-                                   : sum)
         for (i = Nstart; i < Nfinish; i++)
         {
             double x = (i + 0.5) * step;
@@ -23,19 +20,37 @@ void pi_r(long Nstart, long Nfinish, double step)
     else
     {
         iblk = Nfinish - Nstart;
-        pi_r(Nstart, Nfinish - iblk / 2, step);
-        pi_r(Nfinish - iblk / 2, Nfinish, step);
+#pragma omp task shared(sum1)
+        {
+            sum1 = pi_r(Nstart, Nfinish - iblk / 2, step, sum);
+        }
+#pragma omp task shared(sum2)
+        {
+            sum2 = pi_r(Nfinish - iblk / 2, Nfinish, step, sum);
+        }
+#pragma omp taskwait
+        {
+            sum = sum1 + sum2;
+        }
     }
+    return sum;
 }
 
 int main()
 {
     long i;
+    double sum = 0.0;
     double step, pi;
     double init_time, final_time;
     step = 1.0 / (double)num_steps;
     init_time = omp_get_wtime();
-    pi_r(0, num_steps, step);
+#pragma omp parallel
+    {
+#pragma omp master
+        {
+            sum = pi_r(0, num_steps, step, sum);
+        }
+    }
     pi = step * sum;
     final_time = omp_get_wtime() - init_time;
 
