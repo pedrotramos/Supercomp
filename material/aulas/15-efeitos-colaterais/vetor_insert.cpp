94d868a79b7092f0ce519806ee6ff7ce7ab31b81
Pedro Ramos <pedrotramos97@gmail.com>
2021-05-07 14:01:30 -0300

    Aula Efeitos Colaterais

--- material/aulas/15-efeitos-colaterais/vetor_insert.cpp
+++ material/aulas/15-efeitos-colaterais/vetor_insert.cpp
@@ -1,22 +1,31 @@
 #include <vector>
 #include <iostream>
 #include <cstdlib>
+#include <omp.h>
+#include <unistd.h>
 
-double conta_complexa(int i) {
+double conta_complexa(int i)
+{
 	sleep(1);
 	return 2 * i;
 }
 
-int main() {
-	int N = 10000; 
+int main()
+{
+	int N = 100;
 	std::vector<double> vec;
-	for (int i = 0; i < N; i++) {
-		vec.push_back(conta_complexa(i));
+	vec.reserve(vec.size() + N);
+#pragma omp parallel for
+	for (int i = 0; i < N; i++)
+	{
+		double val = conta_complexa(i);
+		vec[i] = val;
 	}
-	
-	for (int i = 0; i < N; i++) {
+
+	for (int i = 0; i < N; i++)
+	{
 		std::cout << i << " ";
 	}
-	
+
 	return 0;
 }
